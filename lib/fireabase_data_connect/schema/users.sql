0039361ed30dbd6a857924d73cf387911cca65da
Luiza-CLopes <luizacaissutti@gmail.com>
2025-10-12 19:21:03 -0300

    feature: schemas for database

--- lib/fireabase_data_connect/schema/users.sql
+++
@@ -1,9 +0,0 @@
--- defines users table
-CREATE TABLE users (
-  userId STRING NOT NULL,             -- uid from Firebase Auth
-  userName STRING NOT NULL,                    
-  userEmail STRING NOT NULL,                  
-  userPassword STRING NOT NULL,      -- hashed password
-  userCreateAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- creation date 
-  PRIMARY KEY(userId)
-);
